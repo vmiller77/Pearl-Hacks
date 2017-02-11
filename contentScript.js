b4f613bc1a179858bc8fe235e62a229742da2a92
vmiller77 <vnm@live.unc.edu>
2017-02-11 15:29:41 -0500

    update

---
+++ contentScript.js
@@ -0,0 +1,13 @@
+ document.write(new Date().toLocaleDateString());
+ var woman=[];
+
+	var graceHopper = {name:"Grace Hopper",
+	field:"Computer Science/Math",
+	description:"Born in New York City in 1906, Grace Hopper joined the U.S. Navy during World War II and was assigned to program the Mark I computer. She continued to work in computing after the war, leading the team that created the first computer language compiler, which led to the popular COBOL language."};
+	var saumyRay = {name:"Saumya Ray",
+	field:"Computer Science/Information Science",
+	description: "Saumya really loves pita!"
+	}
+	woman.push(graceHopper);
+	woman.push(saumyRay);
+	document.write(woman[0].name);
