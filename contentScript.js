96a90fcebde078b47c5feae0a263674be13e5125
SaumyashreeRay <saumyaray1@gmail.com>
2017-02-11 16:20:28 -0500

    CSS updates

--- contentScript.js
+++ contentScript.js
@@ -5,9 +5,10 @@
 	description:"Born in New York City in 1906, Grace Hopper joined the U.S. Navy during World War II and was assigned to program the Mark I computer. She continued to work in computing after the war, leading the team that created the first computer language compiler, which led to the popular COBOL language."};
 	
 	var saumyRay = {name:"Saumya Ray",
-	field:"Computer Science/Information Science",
+	field:"Computer Science",
 	description: "Saumya really loves nail art!"
 	}
 	woman.push(graceHopper);
 	woman.push(saumyRay);
-	document.write(woman[0].name);
+	document.write(woman[0].name + "\n");
+	document.write(woman[0].field);
