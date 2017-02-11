76602f69eff46166dd7e6000799cdbb76af5590a
SaumyashreeRay <saumyaray1@gmail.com>
2017-02-11 16:53:55 -0500

    cc

--- contentScript.js
+++ contentScript.js
@@ -4,19 +4,31 @@
 	field:"Computer Science/Math",
 	description:"Born in New York City in 1906, Grace Hopper joined the U.S. Navy during World War II and was assigned to program the Mark I computer. She continued to work in computing after the war, leading the team that created the first computer language compiler, which led to the popular COBOL language."};
 	
-	var saumyRay = {name:"Saumya Ray",
+	var saumyaRay = {name:"Saumya Ray",
 	field:"Computer Science",
 	description: "Saumya really loves nail art!"
 	}
+
+	var maggieHamilton = {name:"Margaret Hamilton",
+	field:"Computer Science/Math",
+	description: "Margaret Heafield Hamilton is an American computer scientist, systems engineer, and business owner. She was Director of the Software Engineering Division[3] of the MIT Instrumentation Laboratory, which developed on-board flight software for the Apollo space program."};
+	
+
+	var victoriaMiller = {name:"Victoria Miller",
+	field: "Computer Science/Mathematical Decision Sciences",
+	description: "She's in a business frat but doesn't major in it."}
+
 	woman.push(graceHopper);
-	woman.push(saumyRay);
+	woman.push(saumyaRay);
+	woman.push(victoriaMiller)
+	woman.push(maggieHamilton)
 
 var d = new Date(); 
 var t = d.getTime(); 
-var days = Math.floor(t / (1000));
+var days = Math.floor(t / (50000));
 
 var i = days % woman.length; 
 
 var today = woman[i].name;
 
-	document.write(today);
+document.write(today);
