2b00f0f5f63701455a5336e6c1bc382276362010
vmiller77 <vnm@live.unc.edu>
2017-02-11 19:42:16 -0500

    update

--- contentScript.js
+++ contentScript.js
@@ -10,48 +10,23 @@ var woman=[];
 
 	var graceHopper = {name:"Grace Hopper",
 	field:"Computer Science/Math",
-<<<<<<< HEAD
 	img:"https://upload.wikimedia.org/wikipedia/commons/a/ad/Commodore_Grace_M._Hopper,_USN_(covered).jpg",
-=======
-	img:url("https://upload.wikimedia.org/wikipedia/commons/a/ad/Commodore_Grace_M._Hopper,_USN_(covered).jpg")
->>>>>>> origin/master
 	description:"Born in New York City in 1906, Grace Hopper joined the U.S. Navy during World War II and was assigned to program the Mark I computer. She continued to work in computing after the war, leading the team that created the first computer language compiler, which led to the popular COBOL language."};
 	
 	var saumyaRay = {name:"Saumya Ray",
 	field:"Computer Science",
-<<<<<<< HEAD
-	img:url("https://scontent-iad3-1.xx.fbcdn.net/v/t1.0-9/12509093_821351854657291_4769766298998474702_n.jpg?oh=6e0c840f93fda1cc634b7ebfe4221108&oe=593D802A"),
-=======
-<<<<<<< HEAD
 	img:"https://scontent-iad3-1.xx.fbcdn.net/v/t1.0-9/12509093_821351854657291_4769766298998474702_n.jpg?oh=6e0c840f93fda1cc634b7ebfe4221108&oe=593D802A",
-=======
-	 img:url("https://scontent-iad3-1.xx.fbcdn.net/v/t1.0-9/12509093_821351854657291_4769766298998474702_n.jpg?oh=6e0c840f93fda1cc634b7ebfe4221108&oe=593D802A"),
->>>>>>> origin/master
->>>>>>> origin/master
 	description: "Saumya really loves nail art!"
 	}
-	
 
 	var maggieHamilton = {name:"Margaret Hamilton",
 	field:"Computer Science/Math",
-<<<<<<< HEAD
 	img:"https://upload.wikimedia.org/wikipedia/commons/6/68/Margaret_Hamilton_1995.jpg",
-=======
-	img:url("https://upload.wikimedia.org/wikipedia/commons/6/68/Margaret_Hamilton_1995.jpg"),
->>>>>>> origin/master
 	description: "Margaret Heafield Hamilton is an American computer scientist, systems engineer, and business owner. She was Director of the Software Engineering Division[3] of the MIT Instrumentation Laboratory, which developed on-board flight software for the Apollo space program."};
 
 	var victoriaMiller = {name:"Victoria Miller",
 	field: "Computer Science/Mathematical Decision Sciences",
-<<<<<<< HEAD
-	img: url("https://scontent-iad3-1.xx.fbcdn.net/v/t1.0-9/13697050_10207337430054771_3154755651596441054_n.jpg?oh=1d0b1032b6c69c4de0f96144e275ab70&oe=5901AACF"),
-=======
-<<<<<<< HEAD
 	img:"https://scontent-iad3-1.xx.fbcdn.net/v/t1.0-9/13697050_10207337430054771_3154755651596441054_n.jpg?oh=1d0b1032b6c69c4de0f96144e275ab70&oe=5901AACF",
-=======
-	img: url("https://scontent-iad3-1.xx.fbcdn.net/v/t1.0-9/13697050_10207337430054771_3154755651596441054_n.jpg?oh=1d0b1032b6c69c4de0f96144e275ab70&oe=5901AACF")
->>>>>>> origin/master
->>>>>>> origin/master
 	description: "She's in a business frat but doesn't major in it."}
 
 	var srihitaBongu = {name:"Srihita Bongu",
@@ -112,29 +87,32 @@ nameBoxHeader.style.display="block";
 nameBoxHeader.style.fontSize="1.5em";
 nameBoxHeader.style.margin="0.83em";
 nameBoxHeader.style.fontWeight="bold";
+nameBoxHeader.style.textAlign="center";
 var nameText=document.createTextNode(today);
 nameBoxHeader.appendChild(nameText);
 nameBox.appendChild(nameBoxHeader);
 var box = document.getElementsByClassName("box");
 box[0].appendChild(nameBox);
 
-<<<<<<< HEAD
+var imgBoxHeader=document.createElement("H");
 var img = document.createElement("IMG");
+imgBoxHeader.appendChild(img);
 img.src=woman[i].img;
 img.style.height="200px";
-box[1].appendChild(img);
-=======
-var imgBox = document.createElement("DIV");
-var imgBoxHeader=document.createElement("H");
-imgBoxHeader.style.display="block"; 
-imgBoxHeader.style.fontSize="1.5em";
-imgBoxHeader.style.margin="0.83em";
-imgBoxHeader.style.fontWeight="bold";
-var imgText=document.createTextNode(todayImg);
-imgBoxHeader.appendChild(imgText);
-imgBox.appendChild(imgBoxHeader);
-box[1].appendChild(nameBox);
->>>>>>> origin/master
+box[1].appendChild(imgBoxHeader);
+imgBoxHeader.style.paddingLeft="100px"
+
+// var imgBox = document.createElement("DIV");
+// var imgBoxHeader=document.createElement("H");
+// imgBoxHeader.style.display="block"; 
+// imgBoxHeader.style.fontSize="1.5em";
+// imgBoxHeader.style.margin="0.83em";
+// imgBoxHeader.style.fontWeight="bold";
+// var imgText=document.createTextNode(todayImg);
+// imgBoxHeader.appendChild(imgText);
+// imgBox.appendChild(imgBoxHeader);
+// box[1].appendChild(nameBox);
+
 
 var desBox = document.createElement("DIV");
 var desBoxHeader=document.createElement("H");
